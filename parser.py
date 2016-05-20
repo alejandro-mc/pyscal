ae3bfaae9fd1c003e1f29ad24998d4d3a481df4a
alejandro-mc <morejoncortina@gmail.com>
2016-05-20 19:47:22 -0400

    Adds if satement and comparison operations to  the parser.

--- parser.py
+++ parser.py
@@ -2,17 +2,37 @@
 #Alejandro Morejon Cortina 2016
 
 import scanner as sc
+import sys
 
 #try parsing test file
 
 
 #required datastructures
-symbtab = []
+symbtab = []      #symbol table this is very ineficient MUST CHANGE
 lhs = ''
-next_address=0
+next_address=0    #memory address pointer
+ip =0             #instruction pointer for now it's going to hold the index of the entry
+                  #in the instruction list
+instructions = [] #this instruction list is temporary eventually should write directly to bin
+
+
+def addNewInstruction(instruction):
+    '''
+    instruction is of the form ["operation","parameter"]
+    '''
+    global ip
+    global instructions
+    instructions.append(instruction)
+    ip += 1 #increment must change to the actual opcode size when dealing with real instructions
+
+def write_instructions_to_file():
+    global instructions
+    for instruction in instructions:
+      print(instruction)
 
 def Program():
   global fi
+  global instructions
   #import pdb; pdb.set_trace()
   sc.get_token(fi)
   #import pdb; pdb.set_trace()
@@ -26,8 +46,8 @@ def Program():
     #import pdb; pdb.set_trace()
     statements()
     match(["keyword","endprog"])#program ends
-    print("halt")#produce opcode to halt the program
-
+    addNewInstruction(["halt",""])#produce opcode to halt the program
+    write_instructions_to_file()
 
 def declarations():
     var_declarations()
@@ -85,35 +105,100 @@ def begin():
 
 
 def statements():
-  global lhs
-
   while True:
+      statement()
+      if sc.__CURRENT_TOKEN__ == ["keyword","endprog"]:
+        return
+
+def statement():
+    global lhs
+
+    if sc.__CURRENT_TOKEN__ == ["keyword","if"]:
+      if_statement()
     #so far identifier is only found at the begining of asignments or array idexing
     if sc.__CURRENT_TOKEN__[0] == "identifier":
       lhs = sc.__CURRENT_TOKEN__[1]#save the identifier name
       match(["identifier",""])
       if sc.__CURRENT_TOKEN__ == ["opassign",":="]:
         match(["opassign",":="])
-        Exp()
+        Lexp()
+
         #after the code produced by Exp is evaluated
         #we must put the result from the top of the stack
         #in the correct mem location
-        opstring = "pop %d" % list(filter(lambda x: x['name'] == lhs,symbtab))[0]['address']
-        print(opstring)
+        opstring = ["pop" , list(filter(lambda x: x['name'] == lhs,symbtab))[0]['address']]
+        addNewInstruction(opstring)
         match(["semicolon",";"])
-      if sc.__CURRENT_TOKEN__ == ["keyword","endprog"]:
-        return
 
 
-#for now will only parse arithmetic expression
-#def Goal():
-#    global fi
-#    #import pdb; pdb.set_trace()
-#    sc.get_token(fi)
-#    match(["keyword","program"])
-#    Exp();
-#    match(["keyword","end"])
+def if_statement():
+  match(["keyword","if"])
+  Lexp() #first we must evaluate the logical expression
+         #when the expression is evaluated the result will be at the top of the stack
+  match(["keyword","then"])
+  #then we will jump to some address if the result is not true
+  hole1 = ip #but first we must save ip because we don't know the jump address yet
+  addNewInstruction(["jfalse",""]) #once we know the address we will add the parameter
+  statement()
+
+  #after the first statement we could have an else
+  if sc.__CURRENT_TOKEN__ == ["keyword","else"]:
+     match(["keyword","else"])
+
+     #if there is an else the current instruction will be a jmp so we must
+     #skip it that's why we have ip + 1
+     instructions[hole1][1] = ip + 1
+
+     #as before this is a jump forward so we must save the instruction
+     #pointer to add the jump address later
+     hole2 = ip
+     addNewInstruction(["jmp",""])
+     statement()
+     instructions[hole2][1] = ip
+  else:
+     #if there is no else the program can continue executing
+     #from the current instruction 
+     instructions[hole1][1] = ip
+
+
+
     
+######################
+#EXPRESSION PARSING
+######################
+
+#GRAMMAR (Fixed to void left recursion)
+# L  -> E L'
+# L' -> < E L' | > E L' ....
+# E  -> T E'
+# E' -> + T E' | - T E' ...
+# T  -> F T'
+# T' -> * F T' | div F T' ...
+
+
+def Lexp():
+    Exp()
+    Lprime()
+
+def Lprime():
+    if sc.__CURRENT_TOKEN__ == ["opless","<"]:
+       match(["opless","<"])
+       Exp()
+       addNewInstruction(["less",""])
+       Lprime()
+    elif sc.__CURRENT_TOKEN__ == ["opgreater",">"]:
+       match(["opgreater",">"])
+       Exp()
+       addNewInstruction(["greater",""])
+       Lprime()
+    elif sc.__CURRENT_TOKEN__ == ["opeq","="]:
+       match(["opeq","="])
+       Exp()
+       addNewInstruction(["eq",""])
+       Lprime()
+    #must add the other cases later
+
+
 def Exp():
     Term()
     Eprime()
@@ -122,12 +207,12 @@ def Eprime():
     if sc.__CURRENT_TOKEN__ == ["opplus","+"]:
        match(["opplus","+"])
        Term()
-       print("opadd")
+       addNewInstruction(["opadd",""])
        Eprime()
     elif sc.__CURRENT_TOKEN__ == ["opminus","-"]:
        mathc(["opminus","-"])
        Term()
-       print("opsub")
+       addNewInstruction(["opsub",""])
        Eprime()
  
 def Term():
@@ -138,12 +223,12 @@ def Tprime():
     if sc.__CURRENT_TOKEN__ == ["opmul","*"]:
        match(["opmul","*"])
        Fact()
-       print("opmul")
+       addNewInstruction(["opmul",""])
        Tprime()
     elif sc.__CURRENT_TOKEN__ == ["keyword","div"]:
        match(["keyword","div"])
        Fact()
-       print("opdiv")
+       addNewInstruction(["opdiv",""])
        Tprime()
 
 def Fact():
@@ -157,12 +242,12 @@ def Fact():
 def Id():
     #lookup identifier in the symbol table
     id_address = list(filter(lambda x: x['name'] == sc.__CURRENT_TOKEN__[1],symbtab))[0]['address']
-    opstring = "push %d" % id_address
-    print(opstring)  
+    opstring = ["push" , id_address]
+    addNewInstruction(opstring)  
 
 def Lit():#only numerical literals for now
-    opstring = "pushi %s" % sc.__CURRENT_TOKEN__[1]
-    print(opstring)
+    oppair = ["pushi" , sc.__CURRENT_TOKEN__[1]]
+    addNewInstruction(oppair)
 
 def match(token):
     global fi
@@ -185,7 +270,7 @@ def match(token):
 
 if __name__ == "__main__":
   global fi
-  with open('test1.psc','r') as inputfile:
+  with open(sys.argv[1],'r') as inputfile:
     fi = inputfile
     Program()
 
--- test1.psc
+++ test1.psc
@@ -5,10 +5,14 @@ var a,b,c : integer;
 BEGIN
 
 {this is the best program ever written}
-a := 3 + 5 * 2 ;
+{a := 3 + 5 * 2 ;
 
 b:= 1 + 2;
 
-c:= a div 4;
+c:= a div 4;}
+if 3 > 2 then 
+    c := 1;
+else
+	c := 0;
 
 ENDPROG
